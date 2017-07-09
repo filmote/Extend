523b79ba0081f7c8d143a48122bb79d48a3c2bc0
filmote <simon@bloggingadeadhorse.com>
2017-07-09 23:21:37 +1000

    Initial commit.

--- Extend.ino
+++ Extend.ino
@@ -1,12 +1,11 @@
 #include <Arduboy2.h>
 #include "Arduboy2Ext.h"
 
-Arduboy2Ext arduboy;  
+Arduboy2Ext arduboy;
 
 void setup() {
 
   arduboy.begin();
-  arduboy.setFrameRate(30);
 
 }
 
@@ -14,22 +13,25 @@ void loop() {
 
   arduboy.clear();
 
-  arduboy.setTextWrap(true);
-  arduboy.setCursor(30, 0);
-  arduboy.print("Standard text wrapping at left most position.");
-  arduboy.display();
+  if (mode == 0) {
+    
+    arduboy.setTextWrap(true);
+    arduboy.setCursor(30, 0);
+    arduboy.print("Standard text wrapping at left most position.");
+    arduboy.display();
 
-  arduboy.delayShort(5000);
+  }
+  else {
 
+    arduboy.setTextVertSpacing(9);
+    arduboy.setTextWrap(true, 24, 115);
+    arduboy.setCursor(25, 0);
+    arduboy.print("Wrap between pixels 25 and 120with spacing of9.");
+    arduboy.display();
 
-  arduboy.setTextVertSpacing(9);
+  }
 
-  arduboy.setTextWrap(true, 24, 115);
-  arduboy.setCursor(25, 24);
-  arduboy.print("Wrap between pixels 25 and 120 with spacing of 9.");
-  arduboy.display();
-
-  arduboy.delayShort(5000);
+  arduboy.delayShort(100);
 
 }
 
